c0f87eb98c5829531811a077394fec0ce65a2eb0
Beatwork <beatwork.io@gmail.com>
2025-08-10 14:27:14 +0200

    Delete onyx_mvp_patched directory

--- onyx_mvp_patched/src/pages/api/oauth/start.ts
+++
@@ -1,8 +0,0 @@
-import type { NextApiRequest, NextApiResponse } from "next";
-import { authUrl } from "@/lib/youtube";
-
-export default async function handler(req: NextApiRequest, res: NextApiResponse) {
-  const { clientId } = req.query; // your internal client id
-  const url = authUrl(String(clientId ?? "default"));
-  res.redirect(url);
-}
