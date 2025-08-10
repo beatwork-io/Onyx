63b64b41494c125c89f4709d18231ec311807bf6
Beatwork <beatwork.io@gmail.com>
2025-08-10 14:33:05 +0200

    Add files via upload

---
+++ onyx_full_pg_fix/src/pages/api/oauth/start.ts
@@ -0,0 +1,7 @@
+import type { NextApiRequest, NextApiResponse } from "next";
+import { authUrl } from "@/lib/youtube";
+export default async function handler(req: NextApiRequest, res: NextApiResponse) {
+  const { clientId } = req.query;
+  const url = authUrl(String(clientId ?? "default"));
+  res.redirect(url);
+}
