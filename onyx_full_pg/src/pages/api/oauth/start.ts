68b811b8669161eaa33a73cb3447e9711d6707a0
Beatwork <beatwork.io@gmail.com>
2025-08-10 14:32:39 +0200

    Delete onyx_full_pg directory

--- onyx_full_pg/src/pages/api/oauth/start.ts
+++
@@ -1,7 +0,0 @@
-import type { NextApiRequest, NextApiResponse } from "next";
-import { authUrl } from "@/lib/youtube";
-export default async function handler(req: NextApiRequest, res: NextApiResponse) {
-  const { clientId } = req.query;
-  const url = authUrl(String(clientId ?? "default"));
-  res.redirect(url);
-}
